5fef8a01280337e5b3a797591bb543edc4327455
MamidalaMadhumitha <mamidalamadhumitha66@gmail.com>
2024-05-14 22:09:44 +0530

    Adding the default images for webpage

--- src/srcipt.js
+++ src/srcipt.js
@@ -19,7 +19,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                 </div>
                 <div class="icon">
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>${weatherItem.weather[0].description}</h6>
+                    <h5>${weatherItem.weather[0].description}</h5>
                 
                 </div>`;
     } else { 
@@ -88,6 +88,7 @@ const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
         position => {
             const { latitude, longitude } = position.coords; 
+
             // Getting city name from coordinates using  geocoding API
             const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
             fetch(API_URL).then(response => response.json()).then(data => {
@@ -97,7 +98,8 @@ const getUserCoordinates = () => {
                 alert("An error occurred while fetching the city name!");
             });
         },
-        error => { // Show alert if user denied the location permission
+        // Showing alert if user denied the location permission
+        error => { 
             if (error.code === error.permission_denied) {
                 alert("Geolocation request denied. Please reset location permission to grant access again.");
             } else {
@@ -122,55 +124,3 @@ cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates
 
 
 
-// const Container = document.querySelector('.container');
-
-
-// const Search = document.querySelector('#Search-button');
-// const wheatherBox = document.querySelector('.wheather-box');                                                                                    wheather-box');
-// const wheatherDetails = document.querySelector('.whether-details');
-
-// Search.addEventListener ('click',() => {
-//     const APIKey= 'ecce0115d3116d1b0e9a9e6c7f7a8846'
-//     const city = document.querySelector('.Search-button input').value;
-
-//     if(city == '')
-//         return;
-
-//     fetch('https://api.openweathermap.org/data/2.5/weather?q= ${city}&units=metric&appid=${API key}')
-//     .then(response => response.json()).then(json => {
-
-//         const image = document.querySelector('.wheather-box img');
-//         const temperature = document.querySelector('.wheather-box .temperature');
-//         const description = document.querySelector('.wheather-box .description');
-//         const humidity = document.querySelector('.wheather-details .humidity span');
-//         const wind = document.querySelector('.wheather-details .wind span');
-         
-//         switch (json.wheather[0].main) {
-//             case 'clear':
-//                 image.src = 'img/clear.png';                
-//                 break;
-
-//             case 'rain':
-//                 image.src = 'img/rain.png';                
-//                 break;  
-            
-//             case 'Clouds':
-//                 image.src = 'img/cloud.png';                
-//                 break;  
-
-//             case 'Mist':
-//                 image.src = 'img/Mist.png';                
-//                 break;  
-
-//             case 'Haze':
-//                 image.src = 'img/Mist.png';                
-//                 break;  
-              
-        
-//             default:
-//               image .src="images/cloudy.webp"
-//         }
-
-//     });
-
-// });
